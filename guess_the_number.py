587758e9a7fcabb924235ed5e5034e9f20717eb6
Sergei Shcherbak <sshch1987@gmail.com>
2022-11-03 17:11:09 +0200

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,9 +1,6 @@
 import random
 
 
-digits_written = 0
-
-
 def welcome():
     print("Welcome to 'Guess the number'!\nPlease,type in your name: ")
     player_name = input()
@@ -46,35 +43,70 @@ def game_menu():
 
 def game_a():
     number_a = random.randint(0, 10)
+    digits_taken = 0
     print("I am thinking of a number between 0 and 10. Can you guess?")
-    print("Your guess: ")
-    player_guess = int(input())
-    if player_guess < number_a:
-        print("Your digit is too small.")
-    elif player_guess > number_a:
-        print("That's too big.")
+    for digits_taken in range(6):
+        print("Your guess: ")
+        player_guess = int(input())
+        if player_guess < number_a:
+            print("Your digit is too small.")
+        elif player_guess > number_a:
+            print("That's too big.")
+        elif player_guess == number_a:
+            break
+    if player_guess == number_a:
+        digits_taken = str(digits_taken + 1)
+        print(f"Great! You've passed in {digits_taken} guesses.")
+        restart()
+    elif player_guess != number_a:
+        number_a = str(number_a)
+        print(f"Alas. The number was: {number_a}")
+        restart()
 
 
 def game_b():
     number_b = random.randint(0, 100)
+    digits_taken = 0
     print("I am thinking of a number between 0 and 100. Can you guess?")
-    print("Your guess: ")
-    player_guess = int(input())
-    if player_guess < number_b:
-        print("Your digit is too small.")
-    elif player_guess > number_b:
-        print("That's too big.")
+    for digits_taken in range(11):
+        print("Your guess: ")
+        player_guess = int(input())
+        if player_guess < number_b:
+            print("Your digit is too small.")
+        elif player_guess > number_b:
+            print("That's too big.")
+        elif player_guess == number_b:
+            break
+    if player_guess == number_b:
+        digits_taken = str(digits_taken + 1)
+        print(f"Great! You've passed in {digits_taken} guesses.")
+        restart()
+    elif player_guess != number_b:
+        number_a = str(number_b)
+        print(f"Alas. The number was: {number_a}")
+        restart()
 
 
 def game_c():
     number_c = random.randint(0, 1000)
     print("I am thinking of a number between 0 and 1000. Can you guess?")
-    print("Your guess: ")
-    player_guess = int(input())
-    if player_guess < number_c:
-        print("Your digit is too small.")
-    elif player_guess > number_c:
-        print("That's too big.")
+    for digits_taken in range(21):
+        print("Your guess: ")
+        player_guess = int(input())
+        if player_guess < number_c:
+            print("Your digit is too small.")
+        elif player_guess > number_c:
+            print("That's too big.")
+        elif player_guess == number_c:
+            break
+    if player_guess == number_c:
+        digits_taken = str(digits_taken + 1)
+        print(f"Great! You've passed in {digits_taken} guesses.")
+        restart()
+    elif player_guess != number_c:
+        number_a = str(number_c)
+        print(f"Alas. The number was: {number_a}")
+        restart()
 
 
 def restart():
