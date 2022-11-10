e6d25f6913390a09318eb4d384b23f56eea3ed3b
Sergei Shcherbak <sshch1987@gmail.com>
2022-11-10 22:55:00 +0200

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -4,15 +4,24 @@ import os
 def welcome():
     print("Welcome to the 'Tiny Python games shell'!\n"
           "Please, select a game from the list below:\n"
-          " - g 'Guess the number' \n"
-          " - d 'Dragon cave' \n"
+          " - a 'Dodger' \n"
+          " - b 'Bagels' \n"
           " - c 'Coin flip' \n"
+          " - d 'Dragon cave' \n"
+          " - g 'Guess the number' \n"        
           " - h 'Hangman' \n"
+          " - r 'Reversi' \n"
+          " - s 'Sonar Treasure Hunt' \n"
+          " - t 'Tic Tac Toe' \n"
           " - e to exit the program")
 
     choice = str(input())
 
     match choice:
+        case 'a':
+            pass
+        case 'b':
+            pass
         case 'c':
             return os.system("coin_flip.py")
         case 'd':
@@ -23,6 +32,12 @@ def welcome():
             exit()
         case 'h':
             os.system("hangman.py")
+        case 'r':
+            pass
+        case 's':
+            pass
+        case 't':
+            pass
         case _:
             print("Please, use built-in options!")
 
