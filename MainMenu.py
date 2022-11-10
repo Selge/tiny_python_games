ada2389f0b80be23e9d6225c824e7d42f807e7bc
Sergei Shcherbak <sshch1987@gmail.com>
2022-11-10 22:35:20 +0200

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -7,26 +7,24 @@ def welcome():
           " - g 'Guess the number' \n"
           " - d 'Dragon cave' \n"
           " - c 'Coin flip' \n"
-          " - h 'Hangman'"
+          " - h 'Hangman' \n"
           " - e to exit the program")
 
     choice = str(input())
 
-    games = {
-        'c': os.system("coin_flip.py"),
-        'd': os.system("dragon_cave.py"),
-        'g': os.system("guess_the_number.py"),
-        'e': exit(),
-        'h': os.system("hangman.py")
-    }
-
-    for game, file in games.items():
-        print(game, file)
-    #     if choice == game:
-    #         return file
-    # else:
-    #     print("Please, use built-in options!")
-    #     welcome()
+    match choice:
+        case 'c':
+            return os.system("coin_flip.py")
+        case 'd':
+            return os.system("dragon_cave.py")
+        case 'g':
+            return os.system("guess_the_number.py")
+        case 'e':
+            exit()
+        case 'h':
+            os.system("hangman.py")
+        case _:
+            print("Please, use built-in options!")
 
 
 if __name__ == '__main__':
