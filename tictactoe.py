1944d4ed57e3f24cfa91378117ba9f6b668fde4e
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-02 21:58:37 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -7,7 +7,18 @@ def welcome():
 
 
 def draw_board(board):
-    pass
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
+
+def input_player_sign():
+    sign = ''
+    while not (sign == 'X' or sign == 'O'):
+        print('Do you want to be X or O?')
+        sign = input().upper()
 
 
 if __name__ == '__main__':
