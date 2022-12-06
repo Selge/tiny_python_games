7582331c1ee69c158a7ce0090136ee933b9d41a7
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-06 19:42:50 +0200

    Update bagels.py

--- bagels.py
+++ bagels.py
@@ -14,3 +14,23 @@ def get_secret_num():
         secret_num += str(numbers[i])
     return secret_num
 
+
+def get_clues(guess, secret_num):
+    if guess == secret_num:
+        return "You got it!"
+
+    clues = ()
+    for i in range(len(guess)):
+        if guess[i] == secret_num[i]:
+            clues.append("Fermi")
+        elif guess[i] in secret_num:
+            clues.append("Pico")
+
+    if len(clues) == 0:
+        return "Bagels"
+
+    clues.sort()
+    return ' '.join(clues)
+
+if __name__ == '__main__':
+    welcome("Welcome to the 'Bagels'!", bagels)
